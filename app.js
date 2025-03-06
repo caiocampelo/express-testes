c6c4de1bcdd3d9651789e0719cb27d20e15a4b76
Caio Campelo <caio.rcampelo1@gmail.com>
2025-03-06 16:42:35 -0300

    modificado json de favoritos

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const rotaGames = require('./routes/games')
-const cors = require('cors')
 const rotaFavoritos = require('./routes/favoritos')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(cors({origin:"*"}))
